eea6b9cb3e534dc008e120e997e55cfa923fec53
Emmanuel Asiimwe <90935867+asiimwemmanuel@users.noreply.github.com>
2025-05-27 08:57:29 +0300

    about router

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
+import About from './pages/About/About.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
   },
+  {
+    path: '/about',
+    element: <About />,
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
