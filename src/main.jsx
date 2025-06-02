60fdae517560edac76fcc93062d3228ed82417ed
Emmanuel Asiimwe <90935867+asiimwemmanuel@users.noreply.github.com>
2025-06-02 20:51:40 +0300

    digging up my stash

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About/About.jsx";
+import CartPage from "./pages/Cart/Cart.jsx";
 import ContextProvider from "./Context/ContextProvider.jsx";
 import Shop from "./pages/Shop/Shop.jsx";
 import Cart from "./pages/Cart/Cart.jsx";
