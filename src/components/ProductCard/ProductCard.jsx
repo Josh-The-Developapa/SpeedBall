b3358e852e5f09526a6e744283fe853b94d96e16
Joshua Mukisa <kiryowajoshua22@gmail.com>
2025-06-02 13:27:27 +0300

    Updated the ProductCard Component to be more accurate and in accordance with Albert's designs. So styling changes were made, and I implemented the redirect to the individual product page upon clicking on the product title.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,27 +1,36 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import './ProductCard.css';
 
 export default function ProductCard(props) {
+  const navigate = useNavigate();
+  const productFormattedName = props.product.replace(/\s+/g, '-').toLowerCase();
   const [selectedSize, setSelectedSize] = useState('');
 
   const sizes = ['M', 'L', 'XL', '2XL'];
 
   return (
-    <div className="bg-zinc-900 text-white shadow-lg p-4 w-72 space-y-4 product-card">
+    <div className="bg-zinc-900 text-white  p-4 w-72 space-y-4 product-card">
       {/* Image Placeholder */}
       <div className="w-full h-48 bg-zinc-800 rounded-xl flex items-center justify-center image-holder">
         <img
           src={props.image}
-          className="product-image"
+          className="product-card-image"
           style={props.image_dimensions}
         />
       </div>
 
       {/* Product Name */}
-      <h2 className="text-lg font-semibold">{props.product}</h2>
+      <h2
+        className="text-lg font-semibold product-card-title"
+        onClick={() => navigate(`/product/${productFormattedName}`)}
+      >
+        {props.product}
+      </h2>
 
       {/* Price */}
-      <p className="text-lg text-blue-400 font-bold">{props.price}</p>
+      <p className="text-lg text-white-400 font-bold price">{props.price}</p>
 
       {/* Size Selection */}
       <div className="flex flex-wrap gap-2">
@@ -29,7 +38,7 @@ export default function ProductCard(props) {
           <button
             key={size}
             onClick={() => setSelectedSize(size)}
-            className={`px-3 py-1 rounded-md border ${
+            className={`px-3 py-1 rounded-md border sizes ${
               selectedSize === size
                 ? 'bg-blue-500 text-white border-blue-500'
                 : 'border-zinc-600 text-gray-300'
@@ -41,7 +50,7 @@ export default function ProductCard(props) {
       </div>
 
       {/* Add to Cart Button */}
-      <button className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-xl transition">
+      <button className="w-full text-white py-2 rounded-xl transition cart-button">
         ADD TO CART
       </button>
     </div>
